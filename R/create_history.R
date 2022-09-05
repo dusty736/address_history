7ecf068274fccb71cb0ce0121f24c949bee5994c
Dustin Burnham <dusty736@student.ubc.ca>
2022-09-05 17:38:58 -0400

    Add new cleaning functionality

--- R/create_history.R
+++ R/create_history.R
@@ -17,24 +17,7 @@ address_data <- read.csv(address_file)
 # Address Cleaning -------------------------------------------------------------
 ################################################################################
 
-# Collapse redundant data
-address_data <- address_data %>% 
-  mutate(full_address = paste(street, city, zip, state)) %>% 
-  mutate(lead_ppt_id = lead(ppt_id),
-         lead_full_address = lead(full_address),
-         lead_start_date = lead(start_date),
-         lead_end_date = lead(end_date)) %>% 
-  mutate(address_dist = stringdist::stringdist(full_address,
-                                               lead_full_address)) %>% 
-  mutate(end_date = ifelse(ppt_id == lead_ppt_id & address_dist == 0,
-                           lead_start_date,
-                           end_date),
-         updated = ifelse(ppt_id == lead_ppt_id & address_dist == 0,
-                          1,
-                          0)) %>% 
-  select(-lead_ppt_id, -lead_full_address, 
-         -lead_start_date, -lead_end_date, 
-         -address_dist)
+address_data <- format_data(address_data, "ppt_id")
 
 
 
