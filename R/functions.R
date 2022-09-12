4a2d3f438c8fa5a98b2304c73138d7d440c4d264
Dustin Burnham <dusty736@student.ubc.ca>
2022-09-12 09:51:20 -0400

    Create functions for address history

--- R/functions.R
+++ R/functions.R
@@ -146,18 +146,192 @@ format_data <- function(df, id_col) {
   df['start_date'] <- normalize_dates(df[['start_date']])
   df['end_date'] <- normalize_dates(df[['end_date']])
   
+  df <- df %>% 
+    mutate(start_date = as.Date(start_date, origin = "1970-01-01"),
+           end_date = as.Date(end_date, origin = "1970-01-01"),)
+  
   # Order
   df %>% 
     arrange(id_col, start_date, end_date)
+  
+  return(df)
+}
+
+#' Title
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dedup_addresses <- function(df) {
+  
+  # Make sure the data is structured chronologically
+  df <- df %>% 
+    arrange(ppt_id, start_date)
+  
+  # Create running counts of addresses
+  df <- df %>% 
+    group_by(ppt_id, grp = with(rle(paste(address, city, state, zip)), rep(seq_along(lengths), lengths))) %>%
+    mutate(Counter = max(seq_along(grp))) %>%
+    ungroup() %>%
+    select(-grp) %>% 
+    mutate(compare_text = paste(ppt_id, address, city, state, zip),
+           last_compare_text = lag(paste(ppt_id, address, city, state, zip)))
+  
+  # Add column to count address appearance count
+  comb_vec <- c()
+  comb_count <- c()
+  output <- c()
+  
+  for (row in 1:dim(df)[1]) {
+    tmp <- df[row, 'compare_text'][[1]]
+    tmp_prev <- df[row, 'last_compare_text']
+    
+    if (!tmp %in% comb_vec) {
+      comb_vec <- c(comb_vec, tmp)
+      comb_count[eval(tmp)] <- 1
+      output <- c(output, 1)
+    } else if (tmp %in% comb_vec & tmp != tmp_prev) {
+      comb_count[eval(tmp)] <- comb_count[eval(tmp)] + 1
+      output <- c(output, comb_count[tmp])
+    } else {
+      output <- c(output, comb_count[tmp])
+    }
+  }
+  
+  df <- df %>% 
+    mutate(appearance_count = output) %>% 
+    select(-compare_text, -last_compare_text)
+  
+  df <- df %>% 
+    group_by(ppt_id, address, city, state, zip, Counter, appearance_count) %>% 
+    summarize(start_date = min(start_date, na.rm=TRUE),
+              end_date = max(end_date, na.rm=TRUE)) %>% 
+    ungroup() %>% 
+    arrange(ppt_id, start_date)
+  
+  return(df)
+}
+
+#' Title
+#'
+#' @param df 
+#' @param min_date 
+#' @param max_date 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+set_beginning_end <- function(df, min_date = "1960-01-01", max_date = "9999-12-31") {
+  # Add column for consecutive ppt rows
+  df <- df %>% 
+    arrange(ppt_id, start_date) %>% 
+    mutate(consecutive_ppt = sequence(rle(as.character(df$ppt_id))$lengths),
+           comb_addr = paste(address, city, state, zip))
+  
+  unique_address_counts <- df %>% 
+    group_by(ppt_id) %>% 
+    summarize(unique_addresses=n())
+  
+  # Handle the beginning and end of the timeframe
+  df <- df %>% 
+    mutate(consecutive_ppt = sequence(rle(as.character(df$ppt_id))$lengths)) %>% 
+    left_join(., unique_address_counts[c('ppt_id', 'unique_addresses')], by=c('ppt_id')) %>% 
+    mutate(start_date = as.Date(ifelse(consecutive_ppt == 1, as.Date(min_date, format='%Y-%m-%d'), start_date), format='%Y-%m-%d', origin='1970-01-01'),
+           end_date = as.Date(ifelse(consecutive_ppt == unique_addresses, as.Date(max_date, format='%Y-%m-%d'), end_date), format='%Y-%m-%d', origin='1970-01-01'))
+  
+  return(df)
+}
+
+#' Title
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fill_backward <- function(df) {
+  df <- df %>% 
+    mutate(lead_ppt = lead(ppt_id),
+           lead_start_date = lead(start_date),
+           lagged_ppt = lag(ppt_id),
+           lagged_end_date = lag(end_date)) %>% 
+    mutate(start_date = as.Date(ifelse(ppt_id == lagged_ppt & (is.na(start_date) | is.infinite(start_date)) & !is.na(lagged_end_date),
+                                       lagged_end_date + 1,
+                                       start_date),
+                                origin="1970-01-01"))
+  
+  return(df)
+}
+
+#' Title
+#'
+#' @param df 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fill_forward <- function(df) {
+  df <- df %>% 
+    mutate(lead_ppt = lead(ppt_id),
+           lead_start_date = lead(start_date),
+           lagged_ppt = lag(ppt_id),
+           lagged_end_date = lag(end_date)) %>% 
+    mutate(end_date = as.Date(ifelse(ppt_id == lead_ppt & (is.na(end_date) | is.infinite(end_date)) & !is.na(lead_start_date),
+                                     lead_start_date - 1,
+                                     end_date),
+                              origin="1970-01-01"))
+  
+  return(df)
+}
+
+create_history <- function(df, min_date = "1960-01-01", max_date = "9999-12-31") {
+  
+  df <- df %>% 
+    # Set Date Type
+    mutate(start_date = as.Date(start_date, origin = "1970-01-01"),
+           end_date = as.Date(end_date, origin = "1970-01-01"))
+    
+  
 }
 
 
+test <- address_data %>% 
+  mutate(start_date = as.Date(ifelse(is.infinite(start_date), as.Date(NA), start_date),
+                              origin="1970-01-01"),
+         end_date = as.Date(ifelse(is.infinite(end_date), as.Date(NA), end_date),
+                            origin="1970-01-01")) %>% 
+  mutate(na_count = is.na(start_date) + is.na(end_date))
 
 
 
 
 
 
+  mutate(end_date = as.Date(ifelse(ppt_id == lead_ppt & row != nrow(test) & !is.na(lead_start_date), 
+                           (as.numeric(end_date) + as.numeric(lead_start_date)) / 2, 
+                           end_date),
+                           origin = "1970-01-01"),
+         start_date = as.Date(ifelse(ppt_id == lagged_ppt & row != 1 & !is.na(lagged_end_date), 
+                                           (as.numeric(start_date) + as.numeric(lagged_end_date)) / 2 + 1, 
+                                           start_date),
+                                    origin = "1970-01-01")) %>% 
+  mutate(Counter = sequence(rle(as.character(test$ppt_id))$lengths)) %>% 
+  group_by(ppt_id) %>% 
+  mutate(appearance_count = n()) %>% 
+  ungroup() %>% 
+  mutate(end_date = as.Date(ifelse(Counter == appearance_count & is.na(end_date),
+                                   Sys.Date(),
+                                   end_date),
+                            origin = "1970-01-01"))
+
+
+         
 
 
 
