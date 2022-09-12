053abba49f85850a3ac7fb967a8841be8f9c71e6
Dustin Burnham <dusty736@student.ubc.ca>
2022-09-12 09:50:58 -0400

    Utilize functions

--- R/create_history.R
+++ R/create_history.R
@@ -7,6 +7,12 @@
 library(tidyverse)
 
 ################################################################################
+# Source Files -----------------------------------------------------------------
+################################################################################
+
+source("./R/functions.R")
+
+################################################################################
 # Read in Data -----------------------------------------------------------------
 ################################################################################
 
@@ -17,12 +23,20 @@ address_data <- read.csv(address_file)
 # Address Cleaning -------------------------------------------------------------
 ################################################################################
 
+# Format Data
 address_data <- format_data(address_data, "ppt_id")
 
+# Remove Duplicate Rows
+address_data <- dedup_addresses(address_data)
 
+# Set min and max dates for first and last addresses
+address_data <- set_beginning_end(address_data)
 
+# Backfill Fill Gaps
+address_data <- fill_backward(address_data)
 
-
+# Forward Fill Gaps
+address_data <- fill_forward(address_data)
 
 
 
